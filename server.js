2f066a3c22f32d1e8dc1fddbb99301ae67ee776a
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-28 17:53:26 +0500

    Added mongoose to the project

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 var morgan = require('morgan');
+var mongoose = require('mongoose');
+
+
+app.use(morgan('dev'))
 
 
 app.listen(port, () => {
