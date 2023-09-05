a711f01d4ac7ea59982ab5cc3971499ee2a11690
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-09-05 18:10:40 +0500

    Added routing path

--- frontend/buy-and-sell/src/app/app-routing.module.ts
+++ frontend/buy-and-sell/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { NgModule } from '@angular/core';
+
+const routes: Routes = [
+  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
