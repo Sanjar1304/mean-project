e5ec4fd5367d77c3906d865f5a524349991888c7
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-09-06 15:07:48 +0500

    Added contact-page component

--- frontend/buy-and-sell/src/app/app.module.ts
+++ frontend/buy-and-sell/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
 import { ListingsDetailPageComponent } from './listings-detail-page/listings-detail-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ListingsPageComponent,
-    ListingsDetailPageComponent
+    ListingsDetailPageComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
