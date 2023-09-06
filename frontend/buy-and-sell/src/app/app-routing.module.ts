069c9c9eab956d539afd3a330711f5ce02c31319
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-09-06 15:32:27 +0500

    Added path routes

--- frontend/buy-and-sell/src/app/app-routing.module.ts
+++ frontend/buy-and-sell/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { EditListingPageComponent } from './edit-listing-page/edit-listing-page.component';
+import { ListingsDetailPageComponent } from './listings-detail-page/listings-detail-page.component';
 import { ListingsPageComponent } from './listings-page/listings-page.component';
+import { MyListingsPageComponent } from './my-listings-page/my-listings-page.component';
+import { NewListingsPageComponent } from './new-listings-page/new-listings-page.component';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  {path: 'listings', component: ListingsPageComponent, pathMatch: 'full'},
+  { path: '', redirectTo: '/listings', pathMatch: 'full' },
+  { path: 'listings', component: ListingsPageComponent, pathMatch: 'full' },
+  { path: 'listings/:id', component: ListingsDetailPageComponent },
+  { path: 'contact/:id', component: ContactPageComponent },
+  { path: 'edit-listing/:id', component: EditListingPageComponent },
+  { path: 'my-listings', component: MyListingsPageComponent},
+  { path: 'new-listing', component: NewListingsPageComponent}
 ];
 
 @NgModule({
