f4afc82c778660dbe5299b4e76a3b55a715b01b8
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-09-07 17:43:47 +0500

    Started new-listings-page component

--- frontend/buy-and-sell/src/app/new-listings-page/new-listings-page.component.ts
+++ frontend/buy-and-sell/src/app/new-listings-page/new-listings-page.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listings-page',
   templateUrl: './new-listings-page.component.html',
   styleUrls: ['./new-listings-page.component.css']
 })
-export class NewListingsPageComponent {
+export class NewListingsPageComponent implements OnInit{
+
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+  constructor(private router: Router){}
+
+
+  ngOnInit():void {}
+
+
+
+  onSubmit(): void{
+    alert(`Creating new listing page...`);
+    this.router.navigateByUrl('/my-listings')
+  }
+
+
+
+
 
 }
