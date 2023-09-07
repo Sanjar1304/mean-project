85c32f5f8ae30962f16a3d37fd0d30032d6c95ac
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-09-07 16:22:53 +0500

    Started styling my-listings-page component

--- frontend/buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
+++ frontend/buy-and-sell/src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
   styleUrls: ['./my-listings-page.component.css']
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit{
+
+
+
+  constructor(){}
+
+
+
+  ngOnInit():void {}
+
+
+  
 
 }
