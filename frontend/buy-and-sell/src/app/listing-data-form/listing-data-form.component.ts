95e65a89d00b0b789f06c71750c0b91da8a694b7
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-09-11 15:23:47 +0500

    Added Input property to listing-data-form comp.

--- frontend/buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
+++ frontend/buy-and-sell/src/app/listing-data-form/listing-data-form.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listing-data-form',
   templateUrl: './listing-data-form.component.html',
   styleUrls: ['./listing-data-form.component.css']
 })
-export class ListingDataFormComponent {
+export class ListingDataFormComponent implements OnInit {
+
+  @Input() buttonText = '';
+
+  name: string = '';
+  description: string = '';
+  price: string = '';
+
+
+  constructor(private router: Router){}
+
+
+  ngOnInit(): void {}
+
+
+
+  onSubmit(): void{
+    alert(`Creating new listing page...`);
+    this.router.navigateByUrl('/my-listings')
+  }
+
+
+
 
 }
